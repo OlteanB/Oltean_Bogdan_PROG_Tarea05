bbbd3846e81155afa5932b61bca6f84a0a569fad
Bogdan <bogdan>
2018-01-30 20:08:11 +0100

    Commit clase Cliente - atributos

---
+++ Oltean_Bogdan_PROG_Tarea05/src/tarea05/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author bogdan
+ */
+public class Cliente {
+
+    private String nombre, dni, direccion, localidad, codigoPostal;
+    private int identificador, numClientes;
+
+ 
+}
