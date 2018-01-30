77d04f3e8c3a92d6c319aadb5cf1f4134d692275
Bogdan <bogdan>
2018-01-30 20:12:58 +0100

    Commit clase Cliente - constructores y metodos comprueba

--- Oltean_Bogdan_PROG_Tarea05/src/tarea05/Cliente.java
+++ Oltean_Bogdan_PROG_Tarea05/src/tarea05/Cliente.java
@@ -17,5 +17,46 @@ public class Cliente {
     private String nombre, dni, direccion, localidad, codigoPostal;
     private int identificador, numClientes;
 
- 
+    public Cliente(String nombre, String DNI, String direccion, String localidad, String codigoPostal) {
+        this.nombre = nombre;
+        //DNI
+        if (compruebaDni(DNI)) {
+            this.dni = DNI;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("Error DNI.");
+        }
+        //direccion
+        this.direccion=direccion;
+        //localidad
+        this.localidad = localidad;
+        //Código postal
+        if (compruebaCodigoPostal(codigoPostal)) {
+            this.codigoPostal = codigoPostal;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("Error código postal.");
+        }
+
+    }
+
+    public Cliente(Cliente cliente) {
+        this.nombre = cliente.nombre;
+        this.dni = cliente.dni;
+        this.direccion = cliente.direccion;
+        this.localidad = cliente.localidad;
+        this.codigoPostal = cliente.codigoPostal;
+    }
+
+    private boolean compruebaDni(String DNI) {
+        Pattern comprueba = Pattern.compile("\\d{8}[A-HJ-NP-TV-Z]");
+        Matcher matcher = comprueba.matcher(DNI);
+        return matcher.matches();
+    }
+
+    private boolean compruebaCodigoPostal(String codigoPostal) {
+        Pattern comprueba = Pattern.compile("0[1-9][0-9]{3}|[1-4][0-9]{4}|5[0-2][0-9]{3}");
+        Matcher matcher = comprueba.matcher(codigoPostal);
+        return matcher.matches();
+    }
+
+
 }
