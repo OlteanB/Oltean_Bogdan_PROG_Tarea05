453f85ae11d9fdfb195dcd6d1a5b5e2d7ef4ea0c
Bogdan <bogdan>
2018-01-30 20:43:47 +0100

    Commit clase Turismo - getters

--- Oltean_Bogdan_PROG_Tarea05/src/tarea05/Turismo.java
+++ Oltean_Bogdan_PROG_Tarea05/src/tarea05/Turismo.java
@@ -42,4 +42,28 @@ public class Turismo {
         return matcher.matches();
     }
 
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
 }
