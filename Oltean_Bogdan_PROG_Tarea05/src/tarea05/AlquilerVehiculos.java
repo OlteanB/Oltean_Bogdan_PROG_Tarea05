aac0396f6efb0c1a5ade0096ec126b2366f51d6a
Bogdan <bogdan>
2018-02-01 23:14:13 +0100

    Commit clase AlquilerVehiculos - metodo addTurismo

--- Oltean_Bogdan_PROG_Tarea05/src/tarea05/AlquilerVehiculos.java
+++ Oltean_Bogdan_PROG_Tarea05/src/tarea05/AlquilerVehiculos.java
@@ -109,5 +109,25 @@ public class AlquilerVehiculos {
             return null;
         }
     }
- 
+    
+    public void addTurismo(Turismo turismo) {
+        int posicion = 0;
+        boolean posicionEncontrada = false;
+        while (posicion < turismos.length && !posicionEncontrada) {
+            if (turismos[posicion] == null) {
+                posicionEncontrada = true;
+            } else if (turismos[posicion].getMatricula().equals(turismo.getMatricula())) {
+                throw new ExcepcionAlquilerVehiculos("Ya existe un vehículo con esa matrícula");
+            } else {
+                posicion++;
+            }
+        }
+        if (posicionEncontrada) {
+            turismos[posicion] = turismo;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("El array de vehículos está lleno.");
+        }
+    }
+
+
 }
