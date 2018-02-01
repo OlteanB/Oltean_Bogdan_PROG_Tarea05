037a934525e52c88ba4359ce0f7bb22210d30116
Bogdan <bogdan>
2018-02-01 23:02:12 +0100

    Commit clase AlquilerVehiculos - atributos

---
+++ Oltean_Bogdan_PROG_Tarea05/src/tarea05/AlquilerVehiculos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+/**
+ *
+ * @author bogdan
+ */
+public class AlquilerVehiculos {
+
+    private Cliente[] clientes;
+    private Turismo[] turismos;
+    private Alquiler[] alquileres;
+
+    private final int MAX_TURISMOS = 20;
+    private final int MAX_CLIENTES = 20;
+    private final int MAX_ALQUILERES = 20;
+
+   
+}
