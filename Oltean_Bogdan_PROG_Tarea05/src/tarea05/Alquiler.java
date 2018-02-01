90e42ef31c81dcaabc0196a102bdc5524bdd900b
Bogdan <bogdan>
2018-02-01 22:49:52 +0100

    Commit clase Alquiler - atributos

---
+++ Oltean_Bogdan_PROG_Tarea05/src/tarea05/Alquiler.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea05;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author bogdan
+ */
+public class Alquiler {
+
+    private Cliente cliente;
+    private Turismo turismo;
+    private Date fecha;
+    private int dias;
+    private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat("dd/MM/yy");
+    private final int MS_DIA = 1000 * 60 * 60 * 24;
+    private final double PRECIO_DIA = 30;
+
+    
+
+}
